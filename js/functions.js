cb95394e527b072cdf7e3a4f7dce85bd21498ddc
Baljit  Singh <baljitsingh@Baljits-MacBook-Air.local>
2017-03-02 00:07:21 +0530

    added contact section

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ $(".imgSection").css("min-height",$(window).height() - 80);
 $(".aboutSection").css("min-height",$(window).height() - 70);
 $(".workSection").css("min-height",$(window).height() - 90);
 $(".quoteSection").css("min-height",$(window).height() - 490);
-$(".contactSection").css("min-height",$(window).height() - 90);
+$(".contactSection").css("min-height",$(window).height() - 190);
 
 
 $(window).scroll(function(){
