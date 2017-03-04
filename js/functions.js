be476ea4513d68758d8229cf17aa19fe864db435
baljit singh <baljitsinghx01@gmail.com>
2017-03-04 16:44:59 +0530

    changed color combination

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 // var height= $(window).height() - 40;
-$(".imgSection").css("min-height",$(window).height() - 80);
-$(".aboutSection").css("min-height",$(window).height() - 70);
+$(".imgSection").css("min-height",$(window).height() - 110);
+$(".aboutSection").css("min-height",$(window).height() - 110);
 $(".workSection").css("min-height",$(window).height() - 90);
 $(".quoteSection").css("min-height",$(window).height() - 490);
 $(".contactSection").css("min-height",$(window).height() - 190);
